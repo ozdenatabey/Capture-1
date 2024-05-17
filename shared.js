1f13d6cb37b037bb08750e113166b46d111125e5
ozdenatabey <ozdenatabey44@gmail.com>
2024-05-17 13:44:52 +0300

    added functionality to sidebar via edit shared.js file

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var modalNoButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 for (i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
@@ -10,7 +12,10 @@ for (i = 0; i < selectPlanButtons.length; i++) {
   });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function () {
+  mobileNav.style.display = "none";
+  closeModal();
+});
 
 modalNoButton.addEventListener("click", closeModal);
 
@@ -18,3 +23,8 @@ function closeModal() {
   backdrop.style.display = "none";
   modal.style.display = "none";
 }
+
+toggleButton.addEventListener("click", function () {
+  mobileNav.style.display = "block";
+  backdrop.style.display = "block";
+});
